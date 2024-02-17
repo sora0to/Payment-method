e9ee5c9e8aebefd713118026a7971e0a2bfb08bf
Serhii Vasyliev <vasiliev.hub@gmail.com>
2024-02-17 02:34:30 -0500

    Create new Card

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
-const addPeymentMethod = document.querySelector('.AddPeymentMethod');
-const popup = document.querySelector('.popup');
-const close = document.querySelector('.close');
+document.addEventListener('DOMContentLoaded', function () {
+    const addPeymentMethod = document.querySelector('.AddPeymentMethod');
+    const popup = document.querySelector('.popup');
+    const close = document.querySelector('.close');
 
-addPeymentMethod.addEventListener('click', function () {
-    popup.style.display = 'block';
-});
+    const newCard = document.getElementById('new__card');
+    const submitCard = document.getElementById('submit_card');
 
-close.addEventListener('click', function () {
-    popup.style.display = 'none';
-});
+    // popup
+    addPeymentMethod.addEventListener('click', function () {
+        popup.style.display = 'block';
+    });
 
-window.addEventListener('click', function (event) {
-    if(event.target == popup) {
+    close.addEventListener('click', function () {
         popup.style.display = 'none';
-    }
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target == popup) {
+            popup.style.display = 'none';
+        }
+    });
+
+    // create new card
+    submitCard.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const cardHolder = document.getElementById('card_holder').value;
+        const expiryDate = document.getElementById('expiry_date').value;
+        const cardNumber = document.getElementById('card_number').value;
+        const cvv = document.getElementById('cvv').value;
+
+        const newElement = document.createElement('p');
+
+        newElement.textContent = `Card Holder: ${cardHolder}, Expiry Date: ${expiryDate}, Card Number: ${cardNumber}, CVV: ${cvv}`;
+
+        newCard.appendChild(newElement);
+
+        popup.style.display = 'none';
+    });
 });
